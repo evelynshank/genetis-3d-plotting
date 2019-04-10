502e6030ff6ad7742d6cb5f7302219f8e0abaf41
evelynshank <47612707+evelynshank@users.noreply.github.com>
2019-04-10 16:14:45 -0400

    Interpol
    edited in an attempt to use an interpol to get something resembling a srface plot

--- gainplotter3d.py
+++ gainplotter3d.py
@@ -1,67 +1,61 @@
 #For importing libraries
 import matplotlib
-matplotlib.use('Agg')
+#matplotlib.use('Agg')
 #Made to recognise requests to generate other output formats (specifically png files) 
 #if you want it in a different file format (ex. pdf) you would type 'matplotlib.use('PDF')
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+#import matplotlib.ticker as ticker
 from mpl_toolkits.mplot3d import Axes3D
-import math
 import numpy as np
 import csv
 
+#creating interpol of the data because the z coordinate is being uncooperative
+def Interpol(mat):
+    thetaExp= np.zeros((2*mat.shape[0]-1,mat.shape[1]))
+    
+    for i in range(thetaExp.shape[0]):
+        if i % 2 == 0:
+            thetaExp[i] = mat[(i-1)/2]
+        if i % 2 == 1:   
+            thetaExp[i] = mat[(i-1)/2]
+            
+
 #####################################################
-
-theta=[]
-phi=[]
-gain=[]
-thetarad=[]
-phirad=[]
-xcoord=[]
-ycoord=[]
-zcoord=[]
-
-#adding the data of the first frequency from the csv file to the arrays
+theta=[] 
+phi=[] 
+gain=[] 
+xcoord=[] 
+ycoord=[] 
+zcoord=[] 
+            
 with open('3519run.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
-    next(plots, None)
-    for row in plots:
-        if row != 0:
+    for i, row in enumerate(plots):
+        if i != 0:
             theta.append(float(row[1]))
             phi.append(float(row[2]))
             gain.append(float(row[4]))
 
+theta = np.array(theta)*np.pi/180.
+phi = np.array(phi)*np.pi/180.
 theta = theta[:325]
 phi = phi[:325]
 gain = gain[:325]
 
-#converting into radians
-for i in range(len(theta)):
 
-        thetarad.append(theta[i]*math.pi/180)
-
-        phirad.append(phi[i]*math.pi/180)
-        
 #converting the spherical coordinates to cartesian coordinates        
-xcoord = gain*np.sin(thetarad)*np.cos(phirad)
-ycoord = gain*np.sin(thetarad)*np.sin(phirad)
-zcoord = gain*np.cos(thetarad)
+xcoord = gain*np.sin(theta)*np.cos(phi)
+ycoord = gain*np.sin(theta)*np.sin(phi)
+zcoord = gain*np.cos(theta)
 
-#converting xcoord, ycoord, and zcoord into 2d arrays
-xcoord, ycoord = np.meshgrid(xcoord, ycoord)
-
-
-EmptySq= np.arange(zcoord).reshape(325,325)
-
-EmptySquare= np.ones((325,325))
-x= (EmptySquare*xcoord)
-y= (EmptySquare*ycoord).T
-
+#converting xcoord and ycoord into 2d arrays
+xMesh = np.ones((xcoord.shape[0], xcoord.shape[0]))*xcoord
+yMesh = np.ones((xcoord.shape[0], xcoord.shape[0]))*ycoord
 
 fig = plt.figure(figsize=(13,13))
 
 ax_gainplot = fig.add_subplot(221, projection='3d')
-ax_gainplot.scatter(xcoord, ycoord, zcoord)
+ax_gainplot.plot_surface(xcoord, ycoord, zcoord)
 ax_gainplot.set_title('Gain Plot')
 ax_gainplot.set_xlabel('Theta')
 ax_gainplot.set_ylabel('Phi')
